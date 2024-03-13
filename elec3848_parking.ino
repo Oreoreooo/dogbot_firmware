6dd887d4940a07aa726e3a9784fbec8e3c977125
NFHr <expfoahh@outlook.com>
2024-03-13 12:17:30 +0800

    adding readme.md

--- elec3848_parking.ino
+++ elec3848_parking.ino
@@ -14,6 +14,8 @@
 #define OLED_RESET 28 // 4 // Reset pin # (or -1 if sharing Arduino reset pin)
 
 #define SERIAL Serial
+
+// BT Module on Serial 3 (D14 & D15)
 #define BTSERIAL Serial3
 
 #define LOG_DEBUG
@@ -39,63 +41,14 @@ unsigned long time;
 Motor motor;
 Sensor sensor;
 
-void UART_Control()
+void BT_Control()
 {
   String tempString;
   char BT_Data = 0;
-  // USB data
-  /****
-   * Check if USB Serial data contain brackets
-   */
 
-  if (SERIAL.available())
-  {
-    char inputChar = SERIAL.read();
-    if (inputChar == '(')
-    { // Start loop when left bracket detected
-      tempString = "";
-      inputChar = SERIAL.read();
-      while (inputChar != ')')
-      {
-        tempString = tempString + inputChar;
-        inputChar = SERIAL.read();
-        if (!SERIAL.available())
-        {
-          break;
-        } // Break when bracket closed
-      }
-    }
-    int commaIndex = tempString.indexOf(','); // Split data in bracket (a, b, c)
-    // Search for the next comma just after the first
-    int secondCommaIndex = tempString.indexOf(',', commaIndex + 1);
-    String firstValue = tempString.substring(0, commaIndex);
-    String secondValue = tempString.substring(commaIndex + 1, secondCommaIndex);
-    String thirdValue = tempString.substring(secondCommaIndex + 1); // To the end of the string
-    // if ((firstValue.toInt() > servo_min and firstValue.toInt() < servo_max) and // Convert them to numbers
-    //     (secondValue.toInt() > servo_min and secondValue.toInt() < servo_max))
-    // {
-    //   pan = firstValue.toInt();
-    //   tilt = secondValue.toInt();
-    //   window_size = thirdValue.toInt();
-    // }
-    SERIAL.flush();
-    BTSERIAL.println(tempString);
-    BTSERIAL.println("Done");
-    if (tempString != "")
-    {
-      display.clearDisplay();
-      display.setCursor(0, 0); // Start at top-left corner
-      display.println("Serial_Data = ");
-      display.println(tempString);
-      display.display();
-    }
-  }
-
-  // BT Control
   /*
     Receive data from app and translate it to motor movements
   */
-  // BT Module on Serial 3 (D14 & D15)
   if (BTSERIAL.available())
   {
     BT_Data = BTSERIAL.read();
@@ -172,21 +125,6 @@ void UART_Control()
   }
 }
 
-// void sendVolt()
-// {
-//   newV = analogRead(A0);
-//   if (newV != oldV)
-//   {
-//     if (!Serial3.available())
-//     {
-//       Serial3.println(newV);
-//       Serial.println(newV);
-//     }
-//   }
-//   oldV = newV;
-// }
-
-// Where the program starts
 void setup()
 {
   motor = Motor();
@@ -196,11 +134,8 @@ void setup()
   SERIAL.println("Start");
   motor.STOP();         // Stop the robot
   BTSERIAL.begin(9600); // BT serial setup
-                        // Pan=PL4=>48, Tilt=PL5=>47
-  // servo_pan.attach(48);
-  // servo_tilt.attach(47);
-  //////////////////////////////////////////////
-  // OLED Setup//////////////////////////////////
+
+  // OLED Setup
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
   { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
@@ -212,9 +147,6 @@ void setup()
   display.setCursor(0, 0);             // Start at top-left corner
   display.println("AI Robot");
   display.display();
-
-  // Setup Voltage detector
-  // pinMode(A0, INPUT);
 }
 
 void loop()
@@ -222,9 +154,8 @@ void loop()
   // run the code in every 20ms
   if (millis() > (time + 15))
   {
-    // voltCount++;
     time = millis();
-    UART_Control(); // get USB and BT serial data
+    BT_Control(); // get USB and BT serial data
 
     sensor.read();
 
@@ -234,18 +165,4 @@ void loop()
     Serial.print(sensor.lightR);
     Serial.print(" DISTANCE: ");
     Serial.println(sensor.distance);
-
-    // // constrain the servo movement
-    // pan = constrain(pan, servo_min, servo_max);
-    // tilt = constrain(tilt, servo_min, servo_max);
-
-    // // send signal to servo
-    // servo_pan.write(pan);
-    // servo_tilt.write(tilt);
   }
-  // if (voltCount >= 5)
-  // {
-  //   voltCount = 0;
-  //   sendVolt();
-  // }
-}
---
+++ readme.md
@@ -0,0 +1,19 @@
+# ELEC3848 Group Project - Required Functions
+
+## Sensor Usage
+
+* Gyroscope
+* LaserPING
+* Photoresistor
+
+## Steps
+
+The car starts from a random location of 1M from the wall, perpendicular to the wall.
+
+* [ ] 1. After start, wait for 2 sec.
+* [ ] 2. Move to a location of 25cm from the wall, and wait for 2 sec.
+* [ ] 3. Turn **CW 90°, wait 2 sec → CCW 270°, wait 2 sec → CW 180°, wait 2 sec**.
+* [ ] 4. Measure the distance and angle of the car to the wall, and wait for 2 sec.
+* [ ] 5. Transfer to the parking location.
+* [ ] 6. Gyroscope and/or related control data should be transmitted to the computer.
+* [ ] 7. Final position of the car parked at 5cm from the wall, center to the LED bar and perpendicular to the wall.
